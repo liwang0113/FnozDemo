185a3e5429d32e53fcc32aa860f54d737fd92bcc
Fnoz <fnoz@sina.com>
2015-03-22 22:52:22 +0800

    03.20.01
    01.初始化工程；

---
+++ FnozDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FnozDemo
+//
+//  Created by Fnoz on 15/3/22.
+//  Copyright (c) 2015年 Fnoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FnozDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FnozDemo
+//
+//  Created by Fnoz on 15/3/22.
+//  Copyright (c) 2015年 Fnoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
